cc90843ca9d157554ec25b80a079806957529a49
poojithapaladugula1-droid <poojithapaladugula1@gmail.com>
2025-09-09 01:43:35 -0700

    Add files via upload

---
+++ Error.java
@@ -0,0 +1,13 @@
+package S4;
+
+public class Error {
+public static void main(String arg[]) {
+	int a=10;
+	int b=5;
+	int c=5;
+	int x=a/(b+c);
+	System.out.println("x="+x);
+	int y=a/(b-c);
+	System.out.println("y="+y);
+}
+}
